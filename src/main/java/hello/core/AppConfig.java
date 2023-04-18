7d3cd55e2f4d3a08afa21e295f6945fb1241ced3
Kim Tae Hoon <kingteddy0210@gmail.com>
2023-04-18 16:49:14 +0900

    0418/Configration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -18,16 +18,19 @@ public class AppConfig {
     //Bean 을 달아주면 스프링컨테이너에 등록이 된다.
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(), discountPolicy());
     }
