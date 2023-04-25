a534d5c4e14f93c173f029e3b4ca253a6ccb7d19
Kim Tae Hoon <kingteddy0210@gmail.com>
2023-04-25 19:28:21 +0900

    0425/탐색 위치와 기본스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(// @Component 붙은 애들 스프링컨테이너에 자동등록 해줌
+// @Component 붙은 애들 스프링컨테이너에 자동등록 해줌
+@ComponentScan(
+        //탐색위치 지정
+//        basePackages = "hello.core",
         //컨포넌트 다 뒤져서 스프링빈으로 자동등록을 할때에 뺄 것들을 지정해주는 것
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
